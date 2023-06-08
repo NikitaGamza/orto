05893e4990eefd880e109295cad9afcaa3f176ea
Gamza <gamza-ns@mail.ru>
2023-06-08 18:38:13 +0300

    Initial frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './app.scss';
+function App() {
+  return (
+    <div>
+      <header className="app">
+        <a href="/">OrtoKomi</a>
+      </header>
+      <main>Список товаров</main>
+    </div>
+  );
+}
+
+export default App;
